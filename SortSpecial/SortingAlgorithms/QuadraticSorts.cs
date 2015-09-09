420361ce39e4623c3dd5850266ab557acb23bbe0
あおさめ <aosamesan@gmail.com>
2015-09-09 15:41:42 +0900

    알고리즘 디렉터리를 나눔

---
+++ SortSpecial/SortingAlgorithms/QuadraticSorts.cs
@@ -0,0 +1,73 @@
+﻿using SortingDataType;
+using System;
+
+namespace SortingAlgorithms
+{
+    internal class BubbleSort<T> : Sortable<T> where T : IComparable
+    {
+        public BubbleSort() : base("Bubble Sort") { }
+
+        public override void Sort(CloneableList<T> list)
+        {
+            ResetCount();
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (CompareItems(list[j], list[j + 1]))
+                    {
+                        var tmp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = tmp;
+                    }
+                }
+            }
+        }
+    }
+
+    internal class SelectionSort<T> : Sortable<T> where T : IComparable
+    {
+        public SelectionSort() : base("Selection Sort") { }
+
+        public override void Sort(CloneableList<T> list)
+        {
+            ResetCount();
+            int minIndex = 0;
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                minIndex = i + 1;
+                for (int j = minIndex; j < list.Count; j++)
+                {
+                    if (CompareItems(list[minIndex], list[j]))
+                        minIndex = j;
+                }
+
+                if (CompareItems(list[i], list[minIndex]))
+                {
+                    T tmp = list[i];
+                    list[i] = list[minIndex];
+                    list[minIndex] = tmp;
+                }
+            }
+        }
+    }
+
+    internal class InsertionSort<T> : Sortable<T> where T : IComparable
+    {
+        public InsertionSort() : base("Insertion Sort") { }
+
+        public override void Sort(CloneableList<T> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int j = i + 1;
+                T tmp = list[j];
+                while (--j >= 0 && CompareItems(list[j], tmp))
+                    list[j + 1] = list[j];
+
+                list[j + 1] = tmp;
+            }
+        }
+    }
+}
